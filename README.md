134486de9393b10797ff97f89cf3a47adea0834d
Vagner Clementino <vagner.clementino@gmail.com>
2017-05-09 19:25:57 -0300

    Revisão de execução do script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# get_contribuitor_data
-Repositório para os script que coleta dados de contribuidores de projetos no Github
+# Coleta de Dados de contribuidores
+
+Repositório para os script que coleta dados de contribuidores de projetos no
+Github para fins acadêmicos.
+
+Requisitos:
+ - Python 3
+ - [PyGithub](https://github.com/PyGithub/PyGithub)
+
+Como executar:
+
+```bash
+python3 get_contributor_data.py -i repositorios.txt -o get_contributor_data.csv
+```
+
+Onde:
+
+ - -i: arquivo com a lista de repositórios para a coleta
+ - -o: nome do arquivo *CSV* com o resultado da coleta. Caso o mesmo nome seja
+   informado o conteúdo será **sobreescrito**.
--- get_contributor_data.py
+++ get_contributor_data.py
@@ -7,11 +7,10 @@ from time import sleep
 import configparser
 import codecs
 import csv
-import sys
 from datetime import datetime
 import os
 import argparse
-#import ipdb as pdb
+# import ipdb as pdb
 
 
 def parse_args():
@@ -64,6 +63,8 @@ def get_parser():
                         default="get_contribuitor_data.csv"
                         )
     return parser
+
+
 def print_message(message):
     """Exibe uma mensagem de forma personalizada
 
@@ -85,7 +86,7 @@ try:
     seconds_to_wait = 2
     args = parse_args()
     csv_file_name = args.csv_filename
-    repositories_file =  args.input_filename
+    repositories_file = args.input_filename
 
     with codecs.open(csv_file_name, 'w') as f:
         writer_csv = csv.writer(f,
@@ -101,30 +102,32 @@ try:
                              )
                             )
         with codecs.open(repositories_file, 'r') as repo_file:
-            repo = repo_file.readline()
-            user_message = 'Analisando o repositório {0}.'.format(repo)
-            print_message(user_message)
-            repo_git = git_api.get_repo(repo)
-            for contrib in repo_git.get_contributors():
-                contrib_counter = contrib_counter + 1
-                if contrib.email is not None:
-                    email_counter = email_counter + 1
-                    writer_csv.writerow((email_counter,
-                                        repo,
-                                        contrib.name,
-                                        contrib.email)
-                                        )
-                user_message = ('Esperando {0} segundos para uma nova '
-                                'consulta!'
-                                ).format(seconds_to_wait)
+            # pdb.set_trace()
+            for repo in repo_file:
+                repo = repo.replace('\n', '')
+                user_message = 'Analisando o repositório {0}.'.format(repo)
                 print_message(user_message)
-                sleep(seconds_to_wait)
+                repo_git = git_api.get_repo(repo)
+                for contrib in repo_git.get_contributors():
+                    contrib_counter = contrib_counter + 1
+                    if contrib.email is not None:
+                        email_counter = email_counter + 1
+                        writer_csv.writerow((email_counter,
+                                            repo,
+                                            contrib.name,
+                                            contrib.email)
+                                            )
+                    user_message = ('Esperando {0} segundos para uma nova '
+                                    'consulta!'
+                                    ).format(seconds_to_wait)
+                    print_message(user_message)
+                    sleep(seconds_to_wait)
 
-            user_message = ('Fim da análise do projeto {0}.'
-                            'Total de contribuidores: {1}. '
-                            'Total com e-mail {2}').format(repo,
-                                                           contrib_counter,
-                                                           email_counter)
-        print_message(user_message)
+                user_message = ('Fim da análise do projeto {0}.'
+                                'Total de contribuidores: {1}. '
+                                'Total com e-mail {2}').format(repo,
+                                                               contrib_counter,
+                                                               email_counter)
+                print_message(user_message)
 except Exception as e:
     print_message(e)
